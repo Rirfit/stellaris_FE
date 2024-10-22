8fc4df53119db1febc3f39ae558544936fc0615f
Gustavo Santos Silva <gustavo1.santos4.silva09@gmail.com>
2024-10-21 23:31:56 -0300

    Cadastro pela metade

--- Stellaris/src/pages/Cadastro/index.jsx
+++ Stellaris/src/pages/Cadastro/index.jsx
@@ -1,6 +1,38 @@
+import './style.css'
+
 export const Cadastro = () => {
     return (
-        <h1>Olá mundo, cadastro!</h1>
+        <>
+            <div className="cadastro">
+
+                <h1>Cadastre-se</h1>
+
+                <div className="container">
+
+                    <form method="post">
+                        <label>Nome do responsável</label>
+                        <input type="text" placeholder="Digite seu nome" />
+
+                        <label>CPF</label>
+                        <input type="number" placeholder="Digite seu CPF" />
+
+                        <label>E-mail</label>
+                        <input type="email" placeholder="E-mail" />
+
+                        <label>Senha</label>
+                        <input type="password" placeholder="Digite sua senha" />
+
+                        <label>Confirmar senha</label>
+                        <input type="password" placeholder="Confirme sua senha" />
+                    </form>
+
+                </div>
+
+                <button>Avançar</button>
+
+
+            </div>
+        </>
     )
 }
 
