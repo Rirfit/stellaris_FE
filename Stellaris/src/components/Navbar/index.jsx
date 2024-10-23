b4f7f8be5cf9c01480231e0265e7822fedf58ddc
Caio Barbosa <caiomontebarbosa104@gmail.com>
2024-10-23 19:22:34 -0300

    navBar semiFinalizada

--- Stellaris/src/components/Navbar/index.jsx
+++ Stellaris/src/components/Navbar/index.jsx
@@ -1,19 +1,32 @@
 import '../Navbar/style.css'
 import { Link } from 'react-router-dom'
-import { Icon } from '@iconify-icon/react';
 import foto from '../../assets/react.svg'
 
 const NavBar =()=>{
     return(
-        <nav className="navbar">
+        <nav>
+        <input type="checkbox" id="sidebar-active"/>
+        <label htmlFor="sidebar-active" className="open-sidebar-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="5em" viewBox="0 0 24 24"><path fill="black" d="M4 7a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H5a1 1 0 0 1-1-1m0 5a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H5a1 1 0 0 1-1-1m0 5a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H5a1 1 0 0 1-1-1"/></svg>
+        </label>
+
+
+        <label htmlFor="sidebar-active" id="overlay"></label>
+
+        <div className="links-container">
+            <label htmlFor="sidebar-active" className="close-sidebar-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+            </label>
+
             <div className="logo"><img src={foto} alt="" /></div>
-            <Link className='btn home'></Link>
-            <Link className='btn sobreNos'></Link>
-            <Link className='btn ia'></Link>
-            <Link className='btn jogos'></Link>
-            <Link className='btn blog'></Link>
-            <Link className='btn cadastro'></Link>
-        </nav>
+            <Link className="btn homebtn" to={"/"}></Link> <p className='paragrafoBtn'>Home</p>
+            <Link className="btn sobreNosbtn"></Link> <p className='paragrafoBtn'>Sobre Nós</p>
+            <Link className="btn iabtn"></Link> <p className='paragrafoBtn'>IA</p>
+            <Link className="btn jogosbtn"></Link> <p className='paragrafoBtn'>Jogos</p>
+            <Link className="btn blog"></Link> <p className='paragrafoBtn'>Blog</p>
+            <Link className="btn meuPerfilbtn"></Link> <p className='paragrafoBtn'>Meu Perfil</p>
+        </div>
+    </nav>
     )
 }
 
