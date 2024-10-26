4631cf5ef21cd39f2e9b8638a2a23bb09e755a7c
Rirfit <lilrirfit@gmail.com>
2024-10-25 22:50:32 -0300

    Estilização da pagina home, falta somente colocar os elementos de fundo

--- Stellaris/src/pages/Home/index.jsx
+++ Stellaris/src/pages/Home/index.jsx
@@ -4,58 +4,115 @@ import '../Home/styles.css'
 export const Home = () => {
     return (
         <>
-        <NavBar/>
-        <h1>Olá, mundo</h1>
-        <div className="callToActionUm">
-            <h1>Realize um teste visual de seu filho de forma prática e rápida aqui</h1>
-            <h2>Visão Clara, sonhos maiores</h2>
-            <button>Entrar</button>
-        </div>
+            <NavBar />
+            <main>
 
-        <div className="contentResultado">
-            <h2>Como chegamos nesse resultado?</h2>
-            <div className="contentIA">
-                <img src="" alt="" />
-                <h3></h3>
-                <p>IA que recomenda a armação mais adequada ao seu rosto</p>
-            </div>
+                <div className="callToActionUm">
+                    <h1>
+                        <span>Realize um teste visual de</span><br />
+                        <span>Seu filho de forma prática e</span><br />
+                        <span>Rápida aqui.</span>
+                    </h1>
+                    <h2>Visão Clara, sonhos maiores!</h2>
+                    <button>Entrar</button>
+                </div>
 
-            <div className="contentJogos">
-                <img src="" alt="" />
-                <h3>Jogos & testes simples</h3>
-                <p>Jogos para identificação possíveis problemas de visão da criança!</p>
-            </div>
 
-            <div className="contentGuia">
-                <ul>
-                    <li>
-                        <div className="contentCard"></div>
-                    </li>
-                    <li>
-                        <div className="contentCard"></div>
-                    </li>
-                </ul>
-                
-            </div>
 
-            <div className="contentMarketing">
-                <p>Fique por dentro das novidades da 
-                Stellaris em nossas redes sociais!</p>
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
-            </div>
-            <div className="contentFeedback">
-                <h2>Feedback dos Usuários</h2>
-                
-            </div>
-            <div className="callToActionDois">
-                <h2>Tá esperando o que?</h2>
-                <p>Venha ver</p>
-                <button>Entrar</button>
-            </div>
-        </div>
+                <div className="contentResultado">
+                    <h2>
+                        <span>Como chegamos</span><br />
+                        <span>nesse resultado?</span>
+                    </h2>
+                    <div className="container">
+                        <div className="contentIA">
+                            <img src="../../" alt="" />
+                            <h3>Uso de IA</h3>
+                            <p>IA que recomenda a armação mais adequada ao seu rosto</p>
+                        </div>
+                        <div className="contentJogos">
+                            <img src="" alt="" />
+                            <h3>Jogos & testes simples</h3>
+                            <p>Jogos para identificação possíveis problemas de visão da criança!</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="contentGuia">
+                    <h2>Acompanhe nossos guias e dicas!</h2>
+                    <ul>
+                        <li>
+                            <div className="contentCard">
+                                <img src="" alt="" />
+                                <p>5 maneiras de se acostumar com o uso de óculos</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="contentCard">
+                                <img src="" alt="" />
+                                <p>Comportamentos que podem indicar a probabilidade de um possível problema visual</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div className="contentMarketing">
+                    <p>
+                        <span>Fique por dentro das</span><br />
+                        <span>novidades da</span> <br />
+                        <span><span className="colorChange">Stellaris</span> em nossas redes </span><br />
+                        <span>sociais!</span>
+                    </p>
+                    <div className="contentRedes">
+                        <img src="" alt="Instagram" />
+                        <img src="" alt="Linkedin" />
+                        <img src="" alt="Github" />
+                        <img src="" alt="Tiktok" />
+                    </div>
+                </div>
+
+
+                <div className="contentFeedback">
+                    <h2>Feedback dos Usuários</h2>
+                    <div>
+                        <div className="cardFeedback">
+                            <h4>Jailson O.</h4>
+                            <div><img src="" alt="" /></div>
+                            <p>IA muito útil</p>
+                            <div>★★★★★</div>
+                        </div>
+                        <div className="cardFeedback">
+                            <h4>Rosane</h4>
+                            <div><img src="" alt="" /></div>
+                            <p>Bom site</p>
+                            <div>★★★★★</div>
+                        </div>
+                        <div className="cardFeedback">
+                            <h4>Aurora</h4>
+                            <div><img src="" alt="" /></div>
+                            <p>Gostei demais</p>
+                            <div>★★★★★</div>
+                        </div>
+                        <div className="cardFeedback">
+                            <h4>Júlio</h4>
+                            <div><img src="" alt="" /></div>
+                            <p>Ajudou muito!</p>
+                            <div>★★★★★</div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className="callToActionDois">
+                    <h2>Tá esperando o que?</h2>
+                    <p>Venha ver</p>
+                    <button>Entrar</button>
+                </div>
+
+
+            </main>
         </>
     )
 }
