3b17fc5753b68079d1f03421d9aa749d50f8b412
Pedro-HTS <pedrohtoriani@gmail.com>
2024-10-27 02:05:42 -0300

    Jogos&&Blog
    Adiciona a página de jogos e de blog, adiciona no Routs, as imagens principais, e um component pras game cards

--- Stellaris/src/routes/app.routes.jsx
+++ Stellaris/src/routes/app.routes.jsx
@@ -2,6 +2,8 @@ import Home from '../pages/Home'
 import Login from '../pages/Login';
 import Cadastro from '../pages/Cadastro';
 import CriancaCadastro from '../pages/CadastroCrianca';
+import Jogos from '../pages/SelecaoJogos';
+import Blog from '../pages/Blog';
 
 import { Route, Routes as Switch } from 'react-router-dom';
 export function AppRoutes(){
@@ -11,6 +13,8 @@ export function AppRoutes(){
             <Route path="/login" element={<Login />} />
             <Route path="/cadastro" element={<Cadastro />} />
             <Route path="/cadastroCrianca" element={<CriancaCadastro />} />
+            <Route path="/blog" element={<Blog/>} />
+            <Route path="/jogos" element={<Jogos/>} />
         </Switch>
     )
 }
