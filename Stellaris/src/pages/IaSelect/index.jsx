1531152575e287283110ea98ef0da9d75c0bada6
Gabriel de Oliveira (Rirfit) <lilrirfit@gmail.com>
2024-10-28 11:37:00 -0300

    Revert "Teste1"
    This reverts commit 1b2950ed2d080d40095fcbc04dc060f54150ed68.

--- Stellaris/src/pages/IaSelect/index.jsx
+++ Stellaris/src/pages/IaSelect/index.jsx
@@ -1,37 +1,20 @@
-import React, { useRef } from 'react';
 import NavBar from "../../components/Navbar";
 import "./style.css";
-import { Link } from "react-router-dom";
-
-const handleAbrir = (videoRef) => {
-  // Solicita permissão para acessar a câmera
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-      // Define o stream de vídeo como a fonte do elemento de vídeo
-      if (videoRef.current) {
-        videoRef.current.srcObject = stream;
-      }
-    })
-    .catch(err => {
-      console.error("Erro ao acessar a câmera: ", err);
-    });
-}
 
 export const IA = () => {
-  const videoRef = useRef(null);
-
   return (
-    <div className="pagina">
-      <NavBar />
-      <div className="paginaIA">
-        <h1>Abra sua câmera.</h1>
-        <div className="botoes">
-          <button onClick={() => handleAbrir(videoRef)}>Abrir</button>
+    <>
+      <div className="pagina">
+        <NavBar />
+        <div className="paginaIA">
+          <h1>Abra sua câmera ou selecione um arquivo existente.</h1>
+          <div className="botoes">
+            <button>ARQUIVO</button>
+            <button>CÂMERA</button>
+          </div>
         </div>
       </div>
-      <video ref={videoRef} width="640" height="480" autoPlay></video>
-    </div>
+    </>
   );
 };
-
 export default IA;
